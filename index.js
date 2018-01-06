9a2cac74ad93df2e99ccb16b2f99e6678987a74a
Leo Tu <ll410119@gmail.com>
2018-01-06 21:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   if (shout(string) === string) {
     return "YES INDEED!";
   }
-  if (string === "I love you, too.") {
-    return "I love you, Grandma.";
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
